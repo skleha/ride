6ba816b4f63782b5574dc2b268990e4735b5eaab
Paul <prose0021@gmail.com>
2019-11-19 13:37:51 -0800

    add error handling and test redux for users CRUD

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -2,6 +2,7 @@ import * as APIUtil from '../util/user_api_util';
 
 export const RECEIVE_USER = "RECEIVE_USER";
 export const REMOVE_USER = "REMOVE_USER";
+export const RECEIVE_USER_ERRORS = "RECEIVE_USER_ERRORS";
 
 
 const receiveUser = user => ({
@@ -14,13 +15,19 @@ const removeUser = userId => ({
     userId
 })
 
+const receiveUserErrors = errors => ({
+    type: RECEIVE_USER_ERRORS,
+    errors
+});
+
 export const fetchUser = userId => dispatch => APIUtil.fetchUser(userId)
-    .then( user => {
-        dispatch(receiveUser(user.data))
-    })
+    .then( user => dispatch(receiveUser(user.data)),
+        errors =>  dispatch(receiveUserErrors(errors)))
 
 export const updateUser = req => dispatch => APIUtil.updateUser(req)
-    .then( user => dispatch(receiveUser(user.data)));
+    .then( user => dispatch(receiveUser(user.data)),
+    errors => dispatch(receiveUserErrors(errors)))
 
 export const deleteUser = userId => dispatch => APIUtil.deleteUser(userId)
-    .then( () => dispatch(removeUser(userId)));
+    .then( () => dispatch(removeUser(userId)), 
+        errors =>  dispatch(receiveUserErrors(errors)))
