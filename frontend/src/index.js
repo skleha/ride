00b3903687e64a092f484d65dd0711ec22b903c8
thejenkdog <47646908+thejenkdog@users.noreply.github.com>
2019-11-19 16:09:35 -0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import Root from './root';
 import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
+import { logout } from "./actions/session_actions";
 
 document.addEventListener('DOMContentLoaded', () => {
     let store; 
