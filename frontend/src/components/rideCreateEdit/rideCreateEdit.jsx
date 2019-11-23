8787c61f2c9f8378cd2670069d5d551e64907a65
thejenkdog <thejenkdog@gmail.com>
2019-11-23 14:20:18 -0800

    Style rideCreateEdit first form, align in center

--- frontend/src/components/rideCreateEdit/rideCreateEdit.jsx
+++ frontend/src/components/rideCreateEdit/rideCreateEdit.jsx
@@ -42,7 +42,7 @@ class RideCreateEdit extends React.Component {
             <input
               className="ride-create-edit-input"
               type="text"
-              placeholder="Name"
+              placeholder="My Favorite Ride"
               onChange={this.handleInput("title")}
             />
 
@@ -88,7 +88,7 @@ class RideCreateEdit extends React.Component {
             <div className="ride-edit-create-label">Describe Your Ride</div>
             <textarea
               className="ride-create-edit-textarea"
-              placeholder="Describe your Ride"
+              placeholder="... more curves than a race track!"
               rows="7"
               cols="50"
               onChange={this.handleInput("description")}
@@ -98,7 +98,7 @@ class RideCreateEdit extends React.Component {
               className="ride-edit-create-submit"
               onClick={this.handleSubmit}
             >
-              Create Ride
+              Go To Map
             </div>
           </div>
         </div>
